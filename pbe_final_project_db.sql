0c6cac8cdd52312ad538548e369f2a6daa5d1eea
FarchanS <93101895+FarchanS@users.noreply.github.com>
2021-12-13 00:46:35 +0800

    update table and data

--- pbe_final_project_db.sql
+++ pbe_final_project_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2021 at 02:48 AM
+-- Generation Time: Dec 12, 2021 at 05:33 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -29,10 +29,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `accesscard` (
   `IdAccess` varchar(11) NOT NULL,
-  `IdFasilitas1` varchar(11) NOT NULL,
-  `IdFasilitas2` varchar(11) NOT NULL,
-  `IdFasilitas3` varchar(11) NOT NULL
+  `IdFasilitas1` varchar(11) DEFAULT NULL,
+  `IdFasilitas2` varchar(11) DEFAULT NULL,
+  `IdFasilitas3` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `accesscard`
+--
+
+INSERT INTO `accesscard` (`IdAccess`, `IdFasilitas1`, `IdFasilitas2`, `IdFasilitas3`) VALUES
+('AC0001', 'G001', 'P001', 'P002'),
+('AC0002', 'G001', '', ''),
+('AC0003', 'TN001', 'SP001', '');
 
 -- --------------------------------------------------------
 
@@ -43,9 +52,21 @@ CREATE TABLE `accesscard` (
 CREATE TABLE `fasilitas` (
   `IdFasilitas` varchar(11) NOT NULL,
   `Nama` varchar(30) NOT NULL,
-  `JamBuka` time NOT NULL,
-  `JamTutup` time NOT NULL
+  `JamBuka` varchar(10) NOT NULL,
+  `JamTutup` varchar(10) NOT NULL,
+  `Kapasitas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `fasilitas`
+--
+
+INSERT INTO `fasilitas` (`IdFasilitas`, `Nama`, `JamBuka`, `JamTutup`, `Kapasitas`) VALUES
+('G001', 'Gym', '08:30', '17:30', 0),
+('P001', 'Parkir Mobil', '00:00', '23:59', 500),
+('P002', 'Parkir Motor', '00:00', '23:59', 250),
+('SP001', 'Swimming Pool', '10:00', '18:00', 20),
+('TN001', 'Tennis', '08:00', '17:00', 6);
 
 -- --------------------------------------------------------
 
@@ -59,6 +80,17 @@ CREATE TABLE `kendaraan` (
   `Warna` varchar(20) NOT NULL,
   `IdParkir` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kendaraan`
+--
+
+INSERT INTO `kendaraan` (`NoPlat`, `JenisKendaraan`, `Warna`, `IdParkir`) VALUES
+('B1234EV', 'Sedan', 'Biru', 'PMAG001'),
+('B2345BEP', 'Hilux', 'Abu-abu', 'PMAG002'),
+('B3456TEG', 'Minibus', 'Putih', 'PMBG001'),
+('B4567TES', 'Sedan', 'Merah', 'PMBG002'),
+('B5678TES', 'Sedan', 'Biru', 'PMBG003');
 
 -- --------------------------------------------------------
 
@@ -75,6 +107,14 @@ CREATE TABLE `owner` (
   `Telepon` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `owner`
+--
+
+INSERT INTO `owner` (`IdOwner`, `Nama`, `TempatLahir`, `TanggalLahir`, `Kelamin`, `Telepon`) VALUES
+('001', 'eko', 'Jakarta', '2000-11-30', 'Laki-Laki', '087812345678'),
+('002', 'eka', 'Tangerang', '2009-12-15', 'Laki-Laki', '081212345678');
+
 -- --------------------------------------------------------
 
 --
@@ -83,11 +123,27 @@ CREATE TABLE `owner` (
 
 CREATE TABLE `parkir` (
   `IdParkir` varchar(10) NOT NULL,
-  `Lantai` int(11) NOT NULL,
-  `Block` int(11) NOT NULL,
+  `Block` varchar(11) NOT NULL,
+  `Lantai` varchar(11) NOT NULL,
   `No` int(11) NOT NULL,
   `status` varchar(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `parkir`
+--
+
+INSERT INTO `parkir` (`IdParkir`, `Block`, `Lantai`, `No`, `status`) VALUES
+('PMAG001', 'A', 'G', 1, 'kosong'),
+('PMAG002', 'A', 'G', 2, 'kosong'),
+('PMAG003', 'A', 'G', 3, 'kosong'),
+('PMAG004', 'A', 'G', 4, 'kosong'),
+('PMAG005', 'A', 'G', 5, 'kosong'),
+('PMBG001', 'B', 'G', 1, 'kosong'),
+('PMBG002', 'B', 'G', 2, 'kosong'),
+('PMBG003', 'B', 'G', 3, 'kosong'),
+('PMBG004', 'B', 'G', 4, 'kosong'),
+('PMBG005', 'B', 'G', 5, 'kosong');
 
 -- --------------------------------------------------------
 
@@ -99,19 +155,28 @@ CREATE TABLE `unit` (
   `NoUnit` varchar(6) NOT NULL,
   `Tipe` varchar(10) NOT NULL,
   `Luas` int(11) NOT NULL,
-  `IdOwner` varchar(4) NOT NULL,
-  `IdAksesCard1` varchar(10) NOT NULL,
-  `IdAksesCard2` varchar(10) NOT NULL,
-  `IdAksesCard3` varchar(10) NOT NULL,
-  `IdAksesCard4` varchar(10) NOT NULL,
-  `IdAksesCard5` varchar(10) NOT NULL,
-  `IdKendaraan1` varchar(10) NOT NULL,
-  `IdKendaraan2` varchar(10) NOT NULL,
-  `IdKendaraan3` varchar(10) NOT NULL,
-  `IdKendaraan4` varchar(10) NOT NULL,
-  `IdKendaraan5` varchar(10) NOT NULL,
+  `IdOwner` varchar(4) DEFAULT NULL,
+  `IdAksesCard1` varchar(10) DEFAULT NULL,
+  `IdAksesCard2` varchar(10) DEFAULT NULL,
+  `IdAksesCard3` varchar(10) DEFAULT NULL,
+  `IdAksesCard4` varchar(10) DEFAULT NULL,
+  `IdAksesCard5` varchar(10) DEFAULT NULL,
+  `IdKendaraan1` varchar(10) DEFAULT NULL,
+  `IdKendaraan2` varchar(10) DEFAULT NULL,
+  `IdKendaraan3` varchar(10) DEFAULT NULL,
+  `IdKendaraan4` varchar(10) DEFAULT NULL,
+  `IdKendaraan5` varchar(10) DEFAULT NULL,
   `IuranStatus` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `unit`
+--
+
+INSERT INTO `unit` (`NoUnit`, `Tipe`, `Luas`, `IdOwner`, `IdAksesCard1`, `IdAksesCard2`, `IdAksesCard3`, `IdAksesCard4`, `IdAksesCard5`, `IdKendaraan1`, `IdKendaraan2`, `IdKendaraan3`, `IdKendaraan4`, `IdKendaraan5`, `IuranStatus`) VALUES
+('A-G-1', '45', 45, '002', 'AC0001', '10000', '', '', '', 'B1234EV', 'B2345BEP', '', '', '', 'Clear'),
+('A-G-2', '45', 45, '001', '10000', '', '', '', '', 'B3456TEG', '', '', '', '', 'Clear'),
+('A-G-3', '46', 46, '002', '10000', '', '', '', '', 'B2345BEP', '', '', '', '', 'Outstanding');
 
 -- --------------------------------------------------------
 
@@ -131,9 +196,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`IdUser`, `Nama`, `Pass`, `Role`) VALUES
-('1', 'fata2', '123', 'Admin'),
+('1', 'fata', '123', 'Admin'),
 ('2', 'hanz', '123', 'Admin'),
-('3', 'fathi', '123', 'Admin');
+('3', 'fathi', '123', 'User'),
+('4', 'hanz2', '345', 'User');
 
 --
 -- Indexes for dumped tables
@@ -191,7 +257,6 @@ ALTER TABLE `unit`
   ADD KEY `IdAksesCard4` (`IdAksesCard4`),
   ADD KEY `IdAksesCard5` (`IdAksesCard5`),
   ADD KEY `IdKendaraan1` (`IdKendaraan1`),
-  ADD KEY `IdOwner_2` (`IdOwner`),
   ADD KEY `IdKendaraan2` (`IdKendaraan2`),
   ADD KEY `IdKendaraan3` (`IdKendaraan3`),
   ADD KEY `IdKendaraan4` (`IdKendaraan4`),
@@ -209,39 +274,10 @@ ALTER TABLE `user`
 --
 
 --
--- Constraints for table `accesscard`
---
-ALTER TABLE `accesscard`
-  ADD CONSTRAINT `fasilitas1` FOREIGN KEY (`IdFasilitas1`) REFERENCES `fasilitas` (`IdFasilitas`),
-  ADD CONSTRAINT `fasilitas2` FOREIGN KEY (`IdFasilitas2`) REFERENCES `fasilitas` (`IdFasilitas`),
-  ADD CONSTRAINT `fasilitas3` FOREIGN KEY (`IdFasilitas3`) REFERENCES `fasilitas` (`IdFasilitas`);
-
---
 -- Constraints for table `kendaraan`
 --
 ALTER TABLE `kendaraan`
   ADD CONSTRAINT `parkir` FOREIGN KEY (`IdParkir`) REFERENCES `parkir` (`IdParkir`);
-
---
--- Constraints for table `owner`
---
-ALTER TABLE `owner`
-  ADD CONSTRAINT `owner` FOREIGN KEY (`IdOwner`) REFERENCES `unit` (`IdOwner`);
-
---
--- Constraints for table `unit`
---
-ALTER TABLE `unit`
-  ADD CONSTRAINT `aksescard1` FOREIGN KEY (`IdAksesCard1`) REFERENCES `accesscard` (`IdAccess`),
-  ADD CONSTRAINT `aksescard2` FOREIGN KEY (`IdAksesCard2`) REFERENCES `accesscard` (`IdAccess`),
-  ADD CONSTRAINT `aksescard3` FOREIGN KEY (`IdAksesCard3`) REFERENCES `accesscard` (`IdAccess`),
-  ADD CONSTRAINT `aksescard4` FOREIGN KEY (`IdAksesCard4`) REFERENCES `accesscard` (`IdAccess`),
-  ADD CONSTRAINT `aksescard5` FOREIGN KEY (`IdAksesCard5`) REFERENCES `accesscard` (`IdAccess`),
-  ADD CONSTRAINT `parkir1` FOREIGN KEY (`IdKendaraan1`) REFERENCES `kendaraan` (`NoPlat`),
-  ADD CONSTRAINT `parkir2` FOREIGN KEY (`IdKendaraan2`) REFERENCES `kendaraan` (`NoPlat`),
-  ADD CONSTRAINT `parkir3` FOREIGN KEY (`IdKendaraan3`) REFERENCES `kendaraan` (`NoPlat`),
-  ADD CONSTRAINT `parkir4` FOREIGN KEY (`IdKendaraan4`) REFERENCES `kendaraan` (`NoPlat`),
-  ADD CONSTRAINT `parkir5` FOREIGN KEY (`IdKendaraan5`) REFERENCES `kendaraan` (`NoPlat`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
